bc7e0ac94e918f7e50b3f702ab1a79d13758259b
ryosama <benjamin.poulain@gmail.com>
2019-03-18 11:28:16 +0100

    Update to answer new question in MediaCoder 0.8.57 Example : What is the 1st letter of the word "decoding"? What is the 10st letter of the word "transcoding"?

--- mediacoder_unlocker.pl
+++ mediacoder_unlocker.pl
@@ -19,11 +19,11 @@ do {
 print "Find donation window (id : $mediacoderWindowsDonationId)\n";
 
 # try to answer question
-my $math_result = computeMathElement($mediacoderWindowsDonationId);
-if ($math_result eq '') {
+my $answer = answerQuesion($mediacoderWindowsDonationId);
+if ($answer eq '') {
   die "Could not find math question\n";
 } else {
-  print "Result is $math_result\n";
+  print "Result is $answer\n";
 }
 
 
@@ -32,11 +32,11 @@ my @coords = GetWindowRect($mediacoderWindowsDonationId);
 # set focus on window
 SetFocus($mediacoderWindowsDonationId);
 # send mouse to result box
-MouseMoveAbsPix($coords[0] + 348, $coords[1] + 243);
+MouseMoveAbsPix($coords[0] + 360, $coords[1] + 243);
 # click
 SendLButtonDown(); SendLButtonUp();
 # send result
-SendKeys($math_result);
+SendKeys($answer);
 # send mouse to continue button
 MouseMoveAbsPix($coords[0] + 348, $coords[1] + 280);
 # click
@@ -49,26 +49,31 @@ goto START;
 ################################################################################################################
 sub detectDonationWindow {
   foreach (FindWindowLike(undef, "Continue in ")) {
-    #my $text = GetWindowText($_);
-    #printf "GetWindowText($_) : $text\n";
-    return $_;
+	#my $text = GetWindowText($_);
+	#printf "Debug : GetWindowText($_) : $text\n";
+	return $_;
   }
   return;
 }
 
 
-sub computeMathElement {
-  my $windowId = shift;
-  my @children = GetChildWindows($windowId);
+sub answerQuesion {
+	my $windowId = shift;
+	my @children = GetChildWindows($windowId);
 
-  foreach (@children) {
-    my $text = WMGetText($_);
-    #print "Result for '$_' : '$text'\n" ;
-    if ($text =~ /Let's do a simple math: ([^=]+)=/i) {
-      print "Question is : $1\n";
-      my $result = eval($1);
-      return $result;
-    }
-  }
-  return '';
+	foreach (@children) {
+		my $text = WMGetText($_);
+		#print "Debug : Result for '$_' : '$text'\n" ;
+		if ($text =~ /Let's do a simple math: ([^=]+)=/i) {
+			print "Question is : $1\n";
+			my $result = eval($1);
+			return $result;
+		} elsif ($text =~ /What is the (\d+).+? letter of the word "(.+?)"\?/i) {
+			print "Question is : $text\n";
+			my ($position,$word) = ($1,$2);
+			my @tmp = split //, $word;
+			return $tmp[$position -1];
+		}
+	}
+	return '';
 }
