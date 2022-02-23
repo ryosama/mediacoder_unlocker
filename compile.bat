2512571feca3df1e9ad13a9f3e793b039ebfdafa
Benjamin <benjamin.poulain@coopmcs.com>
2022-02-23 10:14:42 +0100

    Remove .exe from repo and create a release.

---
+++ compile.bat
@@ -0,0 +1,2 @@
+rem Require PAR::Packer module to compile
+pp -o mediacoder_unlocker.exe mediacoder_unlocker.pl
--- pl2exe.bat
+++
@@ -1,2 +0,0 @@
-rem Require PAR::Packer module to compile
-pp -o mediacoder_unlocker.exe mediacoder_unlocker.pl
